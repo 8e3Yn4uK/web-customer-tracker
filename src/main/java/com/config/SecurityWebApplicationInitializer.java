4807b70f32e7c2e14cf4622c5d4b573315c9af16
8e3Yn4uK <sashko.kondratenko@gmail.com>
2019-03-30 00:12:27 +0100

    Spring Security Configuration no xml

---
+++ src/main/java/com/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,10 @@
+package com.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * Created by 8e3Yn4uK on 29.03.2019
+ */
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
